7ab7bf21d746a8b0ba0cf58e429e8a28c35701f2
MarkAnthonySilva <masilva1619@gmail.com>
2022-04-05 00:30:34 -0700

    Update user_model.js

--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -1,7 +1,6 @@
 const { htmlError } = require("../helpers");
 const mongoConnection = require("../mongoConnection");
 const userCollection = mongoConnection.collection("users");
-let statusCode;
 
 const getAllUsersCol = async () => {
   const cursor = userCollection.find();
@@ -16,16 +15,14 @@ const getAllUsersCol = async () => {
 };
 
 const insertUserCol = async (user) => {
-  await userCollection
-    .insertOne(user)
-    .catch((error) => {
-      throw new Error("User could not be added: " + error.message);
-    });
+  await userCollection.insertOne(user).catch((error) => {
+    throw new Error("User could not be added: " + error.message);
+  });
 };
 
 const getUserByEmailCol = async (email) => {
   return await userCollection.findOne({ email }).catch((error) => {
-    throw new Error(error.message)
+    throw new Error(error.message);
   });
 };
 
