a1e4b3efd6f572b2113db7d24539f6d9e0ab1b01
MarkAnthonySilva <masilva1619@gmail.com>
2022-04-04 22:03:38 -0700

    Basic Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
 import GoogleLoginComponent from "./components/GoogleLoginComponent";
-import axios from 'axios';
+import Header from "./components/Header";
+import axios from "axios";
 
 function App() {
-  
   const testClick = async (response) => {
     try {
-      await axios.get(`${process.env.REACT_APP_BASE_BACKEND}/api/user/allUsers`, 
-        { token: response.tokenId},
-        { withCredentials: true })
-      } catch (e) {
-        console.log(e)
-      }
-  }
+      await axios.get(
+        `${process.env.REACT_APP_BASE_BACKEND}/api/user/allUsers`,
+        { token: response.tokenId },
+        { withCredentials: true }
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
+    // style={{backgroundImage:`url(${homePageImage})`}}
     <div>
-      <h1>Basic HomePage</h1>
-      {/* <h2>Base Backend: {process.env.REACT_APP_BASE_BACKEND}</h2> */}
-      <GoogleLoginComponent />
-    <button onClick={ testClick }>CLICK ON ME</button>
+      <Header />
+      <div className="home-layout">
+        {/* <h2>Base Backend: {process.env.REACT_APP_BASE_BACKEND}</h2> */}
+        <GoogleLoginComponent />
+        <button onClick={testClick}>CLICK ON ME</button>
+        {/* <img src={homePageImage} alt='University Students'/> */}
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Header = () => {
+  return (
+    <div className="generic-card header">
+      <h1>Starflow</h1>
+    </div>
+  );
+};
+
+export default Header;
