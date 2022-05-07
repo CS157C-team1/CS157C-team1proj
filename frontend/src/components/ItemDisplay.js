994d7f81585c79a290b5158c0134296c8c2004cb
MarkAnthonySilva <masilva1619@gmail.com>
2022-05-07 12:39:07 -0700

    Center Item buttons

--- frontend/src/components/ItemDisplay.js
+++ frontend/src/components/ItemDisplay.js
@@ -2,24 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ItemDisplay = ({ itemInfo }) => {
-    return(
-        <div className="item-display">
-            {/* {console.log("IMAGE: " + itemInfo.image)} */}
-            {itemInfo.image == null ? (
-               <div className="item-img-size item-empty-image">
-                   <h1>NO <br/>IMAGE<br/> FOUND</h1>
-               </div> 
-            ) : (
-                <image></image>
-            )}
-            <h2>{itemInfo.name}</h2>
-            <h2>${itemInfo.price}</h2>
-            <h3>Type: {itemInfo.type}</h3>
-            <h3>Condition: {itemInfo.condition}</h3>
-            <button className="btn">Add to Cart</button>
-            <button className="wishlist">Add to Wish List</button>
+  return (
+    <div className="item-display">
+      {/* {console.log("IMAGE: " + itemInfo.image)} */}
+      {itemInfo.image == null ? (
+        <div className="item-img-size item-empty-image">
+          <h1>
+            NO <br />
+            IMAGE
+            <br /> FOUND
+          </h1>
         </div>
-    )   
+      ) : (
+        <image></image>
+      )}
+      <h2>{itemInfo.name}</h2>
+      <h2>${itemInfo.price}</h2>
+      <h3>Type: {itemInfo.type}</h3>
+      <h3>Condition: {itemInfo.condition}</h3>
+      <div className="item-btn-div">
+        <button className="btn">Add to Cart</button>
+        <button className="btn-wishlist">Add to Wish List</button>
+      </div>
+    </div>
+  );
 };
 
 ItemDisplay.defaultProps = {
