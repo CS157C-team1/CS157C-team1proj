f97c89a309d6e18806cfd1bc2f9d6442e33564d5
MarkAnthonySilva <masilva1619@gmail.com>
2022-05-07 12:08:44 -0700

    Change Color Scheme

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,7 +9,7 @@ const Header = ({ isUserLoggedOn, updateUser }) => {
 
   const onClickAddItem = () => {
     setShowModal(true);
-    <AddItemModal showModal={showModal} setShowModal={setShowModal} />;
+    // <AddItemModal showModal={showModal} setShowModal={setShowModal} />;
   };
 
   return (
@@ -18,7 +18,7 @@ const Header = ({ isUserLoggedOn, updateUser }) => {
         <div className="navBar">
           <h1>Starflow</h1>
           {isUserLoggedOn && (
-            <button className="btn headerBGColor" onClick={onClickAddItem}>
+            <button className="btn" onClick={onClickAddItem}>
               SELL
             </button>
           )}
@@ -26,7 +26,7 @@ const Header = ({ isUserLoggedOn, updateUser }) => {
         {isUserLoggedOn ? (
           <GoogleLogoutComponent updateUser={updateUser} />
         ) : (
-          <GoogleLoginComponent updateUser={updateUser} classStyle="btn-header" buttonText="Login" />
+          <GoogleLoginComponent updateUser={updateUser} buttonText="Login" />
         )}
       </div>
       <AddItemModal showModal={showModal} setShowModal={setShowModal} />
